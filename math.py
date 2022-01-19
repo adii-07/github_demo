1d4145bcfd8822b3d0c14542bdbfb84cc9cc79c2
Aditya Nitin Pise <67200743+adii-07@users.noreply.github.com>
2022-01-19 19:15:45 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,24 @@
+# Addition
+def add(a,b):
+	return a+b
+
+# Subtraction
+def subtract(a,b):
+	return a-b 						# on master branch
+
+# Multiplication
+def multiply(a,b):
+	return a*b								# on bug1 branch
+
+# Division
+def divide(a,b):
+	if b==0;
+		return DIVIDE_BY_0_ERROR
+	else:
+		return a/b
+
+print("Hello World")
+
+# Square implementation
+def square(x):
+	return x*x
